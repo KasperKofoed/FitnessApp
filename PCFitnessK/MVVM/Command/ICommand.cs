ed0293b0bfcaeca2d38ce3b6655e7a9135b22a25
KasperKofoed <kasperkofoed@live.dk>
2017-12-30 18:49:34 +0100

    + DataStorage Class / Updated Nuget / View Design Rework
    :+1:

--- PCFitnessK/MVVM/Command/ICommand.cs
+++ PCFitnessK/MVVM/Command/ICommand.cs
@@ -1,32 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Input;
 
-namespace PCFitnessK.MVVM.Command
-{
-    public class DelegateCommand : ICommand
-    {
-        private readonly Action _action;
+//namespace PCFitnessK.MVVM.Command
+//{
+//    public class DelegateCommand : ICommand
+//    {
+//        private readonly Action _action;
 
-        public DelegateCommand(Action action)
-        {
-            _action = action;
-        }
+//        public DelegateCommand(Action action)
+//        {
+//            _action = action;
+//        }
 
-        public void Execute(object parameter)
-        {
-            _action();
-        }
+//        public void Execute(object parameter)
+//        {
+//            _action();
+//        }
 
-        public bool CanExecute(object parameter)
-        {
-            return true;
-        }
+//        public bool CanExecute(object parameter)
+//        {
+//            return true;
+//        }
 
-#pragma warning disable 67
-        public event EventHandler CanExecuteChanged;
-#pragma warning restore 67
-    }
-}
+//#pragma warning disable 67
+//        public event EventHandler CanExecuteChanged;
+//#pragma warning restore 67
+//    }
+//}
---
+++ PCFitnessK/MVVM/ViewModels/Hypertrophy/HPullPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using PCFitnessK.MVVM.Models;
+using PCFitnessK.DataStorage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCFitnessK.MVVM.ViewModels.Hypertrophy
+{
+    public class HPullPageViewModel : ViewModelBase
+    {
+        private List<int> _repRange;
+        private HPullDataStorage _dataStorage;
+
+        public HPullPageViewModel()
+        {
+            _repRange = new List<int>() { 8, 12, 15 };
+            _dataStorage = new HPullDataStorage();
+        }
+
+        public List<int> GetRepRange { get { return _repRange; } set { value = _repRange; } }
+        public HPullDataStorage DataStorage { get { return _dataStorage; } set { value = _dataStorage; } }
+
+        //public string ItemIsSelected
+        //{
+
+        //}
+
+    }
+}
