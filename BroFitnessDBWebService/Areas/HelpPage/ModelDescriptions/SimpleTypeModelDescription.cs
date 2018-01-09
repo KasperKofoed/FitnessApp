214b44e157e6b9d73d49df43996dc969a6128ee7
KasperKofoed <kasperkofoed@live.dk>
2018-01-09 22:07:01 +0100

    + Start Implementation of Web Service
    :+1:

---
+++ BroFitnessDBWebService/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BroFitnessDBWebService.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ BroFitnessDBWebService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BroFitnessDBWebService.WebApiApplication" Language="C#" %>
